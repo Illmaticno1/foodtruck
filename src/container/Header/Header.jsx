7a9b173362921145c19cf28c54b572e955e0add1
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-09-10 21:54:43 -0400

    navbar is done.. working on header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+
+import { images } from '../../constants';
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+        lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
+        lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.
+      </p>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
