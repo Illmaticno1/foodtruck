555c6e783e58b39b7fb369484286a94a5e263965
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-09-28 03:59:14 -0400

    added chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chefs Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-conent_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Your Name</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="signature" />
+      </div>
+    </div>
   </div>
 );
 
